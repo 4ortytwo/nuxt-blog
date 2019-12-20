82a0674f64726960d11dd2fdc5dfe91e819061bd
4ortytwo <a.burgan@cheil.com>
2019-12-20 20:07:16 +0100

    add basic routes

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about-page">
+    <p>Lorem ipsum and some random text</p>
+    <p>Lorem ipsum and some random text</p>
+    <p>Lorem ipsum and some random text</p>
+    <p>Lorem ipsum and some random text</p>
+  </div>
+</template>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
