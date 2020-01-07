184640134270c663382975e444278ed34ba0c176
4ortytwo <a.burgan@cheil.com>
2020-01-07 21:57:08 +0100

    add more assets and navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
+    <TheHeader @isSideNavToggle="displaySideNav = !displaySideNav"/>
+    <TheSidenav />
     <nuxt />
   </div>
 </template>
+
+<script>
+  import TheHeader from '@/components/Navigation/TheHeader';
+  import TheSidenav from '@/components/Navigation/TheSidenav';
+
+  export default {
+    components: {
+      TheHeader,
+      TheSidenav
+    },
+    data() {
+      return {
+        displaySideNav: false
+      }
+    }
+  }
+</script>
 
 <style>
 html {
