f63da46dad4f838d2c2510954bb5ad44c022c208
4ortytwo <a.burgan@cheil.com>
2020-01-10 18:14:35 +0100

    add posts page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="posts-page">
-
+    <section class="posts-list">
+      <PostPreview
+        id="1"
+        :thumbnail="menImg"
+        title="Hello there!"
+        previewText="This is my first post"
+      />
+      <PostPreview
+        id="2"
+        :thumbnail="downloadImg"
+        title="Hello there the Second time!"
+        previewText="This is my second post"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwQeUDWcs60whCTdBsBm1IN1LL0w8ObwwPod55I2siEGWnXNT5FA&s"
+        title="Hi, this is my third post!"
+        previewText="This is my third post"
+      />
+    </section>
   </div>
 </template>
 
 <script>
+import PostPreview from '@/components/Posts/PostPreview'
 export default {
+  components: {
+    PostPreview
+  },
+  data() {
+    return {
+      menImg: require('@/assets/images/men.jpg'),
+      downloadImg: require('@/assets/images/downloadImg.jpeg')
+    }
+  }
 
 }
 </script>
@@ -15,5 +44,14 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 }
 </style>
