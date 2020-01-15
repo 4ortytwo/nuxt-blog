a4f589639fab388c868a4ee96e7721750afbd2a5
4ortytwo <a.burgan@cheil.com>
2020-01-15 17:23:50 +0100

    add UI and Admin components

---
+++ components/Admin/AdminPostForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <form @submit.prevent="onSave">
+    <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+
+    <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+
+    <AppControlInput v-model="editedPost.thumbnailLink"
+      >Thumbnail Link</AppControlInput
+    >
+
+    <AppControlInput control-type="textarea" v-model="editedPost.content"
+      >Content</AppControlInput
+    >
+
+    <AppButton type="submit">Save</AppButton>
+
+    <AppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel"
+      >Cancel</AppButton
+    >
+  </form>
+</template>
+
+<script>
+import AppButton from '@/components/UI/AppButton'
+import AppControlInput from '@/components/UI/AppControlInput'
+export default {
+  components: {
+    AppButton,
+    AppControlInput
+  },
+  data() {
+    return {
+      editedPost: {
+        author: '',
+        title: '',
+        thumbnailLink: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    onSave() {
+      // save
+      console.log('thisObject', this.editedPost)
+    },
+    onCancel() {
+      this.$router.push('/admin')
+    }
+  }
+}
+</script>
