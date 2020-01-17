3d43eb62426b676ec6da81ec82842c15c70ba9ed
4ortytwo <a.burgan@cheil.com>
2020-01-17 11:31:55 +0100

    add admin layout, add auth

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <button @click="$router.push('/admin/new-post')">Create Post</button>
+      <AppButton @click="$router.push('/admin/new-post')"
+        >Create Post</AppButton
+      >
     </section>
     <section class="existing-post">
       <h1>Existing Posts</h1>
-      <PostList isAdmin/>
+      <PostList isAdmin />
     </section>
   </div>
 </template>
 
 <script>
+import AppButton from '@/components/UI/AppButton'
 import PostList from '@/components/Posts/PostList'
 export default {
-  components: { PostList }
+  layout: 'admin',
+  components: { AppButton, PostList }
 }
 </script>
 
